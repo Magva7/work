b83a6c8f3747510b1f2ad26e98cd114d89a0a1ae
Magva <magva7@gmail.com>
2022-10-22 05:20:39 +0600

    init timer

---
+++ timer/run_timer_modul_time_pygame_v1.01.bat
@@ -0,0 +1,3 @@
+mode con:cols=10 lines=10
+python timer_modul_time_pygame_v1.01.py
+PAUSE
---
+++ timer/timer_modul_time_pygame_v1.01.py
@@ -0,0 +1,24 @@
+﻿import time
+import pygame
+import os
+
+set_min = int(input('Введите количество минут: '))
+secund = set_min * 60
+
+
+while secund != 0:
+    secund -= 1  # every iteration minus 1 second
+    print(time.strftime("Осталось: %M:%S", time.localtime(secund)))
+    time.sleep(1)  # 1 second delay
+    os.system('CLS')  # clear console
+    
+print('time =)')
+
+# cycle was complete, play music
+pygame.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load('media/daddy_cool.mp3')  # source music
+pygame.mixer.music.play()  # launch
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
